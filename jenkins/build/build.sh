5807b91a8b96afa9401b596ee7397180ade8aaf7
BrunoNReis <analistabrunonunes@gmail.com>
2019-06-23 18:19:09 -0300

    alteracoes.

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -2,4 +2,4 @@
 
 cp -f target/*.jar jenkins/build/
 
-cd /jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
+cd jenkins/build/ && docker-compose -f docker-compose-build.yml build --no-cache
